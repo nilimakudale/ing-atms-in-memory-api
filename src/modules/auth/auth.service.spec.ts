0619a27baa50cda1048902bc5fb9843cd324bbc8
unknown <nilima.kudale@anka.co.in>
2022-05-04 16:20:44 +0530

    Updated test cases for auth service

--- src/modules/auth/auth.service.spec.ts
+++ src/modules/auth/auth.service.spec.ts
@@ -1,5 +1,5 @@
 import { InMemoryDBService } from '@nestjs-addons/in-memory-db';
-import { Logger } from '@nestjs/common';
+import { InternalServerErrorException, Logger, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { Test, TestingModule } from '@nestjs/testing';
 import { User } from '../users/user.entity';
@@ -16,7 +16,7 @@ describe('AuthService', () => {
     name: 'test'
   };
 
-  const user = {id:'1',...testUser}
+  const user = { id: '1', ...testUser }
 
   const JWTServiceProvider = {
     provide: JwtService,
@@ -28,12 +28,13 @@ describe('AuthService', () => {
   const UsersServiceProvider = {
     provide: InMemoryDBService,
     useFactory: () => ({
-      query: jest.fn((email) => {
-        if (email === testUser.email)
-          return [testUser];
-        else return [];
-      }),
-      create: jest.fn(() => { return { testUser } }),
+      query: jest.fn(() => { }),
+      // jest.fn((email) => {
+      //   if (email === testUser.email)
+      //     return [testUser];
+      //   else return [];
+      // }),
+      create: jest.fn(() => { return user }),
     })
   }
 
@@ -69,14 +70,10 @@ describe('AuthService', () => {
   });
 
   it('should validate user', async () => {
-    const data =await authService.validateUser(testUser.email, testUser.password);
-    expect(userService.query).toHaveBeenCalled();
     jest.spyOn(userService, 'query').mockReturnValueOnce([user])
+    const data = await authService.validateUser(testUser.email, testUser.password);
+    expect(userService.query).toHaveBeenCalled();
     expect(data).toBeDefined();
-
-    jest.spyOn(userService, 'query').mockImplementation(() => [user]);
-    expect(authService.validateUser(testUser.email, testUser.password)).toBeDefined();
-
     jest.spyOn(userService, 'query').mockImplementation(() => {
       throw new Error();
     });
@@ -85,32 +82,46 @@ describe('AuthService', () => {
   });
 
   it('should validate user test with invalid user', async () => {
+    jest.spyOn(userService, 'query').mockImplementation(() => []);
     const data = await authService.validateUser('bchdb', testUser.password);
     expect(data).toEqual(null);
   });
 
   it("should generate token(login)", async () => {
-   // jest.spyOn(authService, 'validateUser').mockReturnValueOnce( new Promise(() => user));
-    jest.spyOn(userService, 'query').mockReturnValueOnce([user])
-   const data = await authService.login({ username: testUser.email, password: testUser.password });
-   // expect(authService.validateUser(testUser.email, testUser.password)).toBeDefined();
-    jest.spyOn(userService, 'query').mockReturnValueOnce([user])
-
-    //jest.spyOn(authService, 'validateUser').mockReturnValueOnce( new Promise(() => user));
+    jest.spyOn(authService, 'validateUser').mockReturnValueOnce(Promise.resolve(user));
+    jest.spyOn(userService, 'query').mockImplementation(() => [user]);
+    const data = await authService.login({ username: testUser.email, password: testUser.password });
     expect(data).toBeDefined();
-  //  expect(authService.create(testUser)).rejects.toMatch('Invalid user credentials');
-   // const data = await authService.login({ username: testUser.email, password: testUser.password });
-
   })
 
+
+  it("should throw 401 Unauthorized error when user is not valid in login service", async () => {
+    jest.spyOn(userService, 'query').mockImplementation(() => null);
+    jest.spyOn(authService, 'validateUser').mockReturnValueOnce(Promise.resolve(null));
+    expect(authService.login({ username: testUser.email, password: testUser.password })).rejects.toThrow(UnauthorizedException);
+  })
+
+  it("should throw 500 error when error occurred", async () => {
+    jest.spyOn(userService, 'query').mockImplementation(() => null);
+    jest.spyOn(authService, 'validateUser').mockImplementation(() => { throw new InternalServerErrorException() });
+    expect(authService.login({ username: testUser.email, password: testUser.password })).rejects.toThrow(InternalServerErrorException);
+  })
 
   it("create new user and generate token(signup)", async () => {
+    jest.spyOn(userService, 'create').mockImplementation(() => user);
     const data = await authService.create(testUser);
     expect(data).toBeDefined();
-    jest.spyOn(userService, 'create').mockImplementation(() => {
-      throw new Error();
-    });
-    expect(authService.create(testUser)).rejects.toMatch('error');
   })
+
+  it("test catch error while creating new user", async () => {
+    jest.spyOn(userService, 'create').mockImplementation(() => {
+      throw new InternalServerErrorException('Internal server error');
+    });
+    await expect(authService.create(testUser)).rejects.toThrow(InternalServerErrorException);
+  })
+
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
 
 });
